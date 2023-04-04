9b4d65f27c9d31c7e19a827f038ab02b621f6ff5
Rasaboun <rayane.amie@gmail.com>
2023-04-04 11:54:53 +0200

    Change Pull-to-Refresh Scrolling Background Color

--- weatherapp/pages/_document.tsx
+++ weatherapp/pages/_document.tsx
@@ -4,10 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <meta name="theme-color" content="rgb(219 234 254 / 0)" />
-      <meta name="color-scheme" content="dark light"/>
+      <meta name="theme-color" content="rgb(219 234 254)" />
         </Head>
-      <body className="bg-blue-100 bg-gradient-to-b from-blue-100  to-blue-300 h-full">
+      <body className="bg-blue-100 bg-gradient-to-b from-blue-100  to-blue-400 min-h-full">
         <Main />
         <NextScript />
       </body>
