c9307c11384cfff93cd8b74421008aa5376a9f7e
Rasaboun <rayane.amie@gmail.com>
2023-04-08 18:10:48 +0200

    New Test Hydratation error with UTC Date offset

--- weatherapp/components/TodayDashBoard.tsx
+++ weatherapp/components/TodayDashBoard.tsx
@@ -19,7 +19,7 @@ export  const TodayDashBoard = ({
 
 
 	const todayDate = new Date();
-  console.log("hi time OFFSET is : ",todayDate.getTimezoneOffset())
+  console.log("hi time OFFSET is : ",todayDate.getUTCMinutes())
 	const actualHours = todayDate.getUTCHours();
 
 	let sunrise = "";
