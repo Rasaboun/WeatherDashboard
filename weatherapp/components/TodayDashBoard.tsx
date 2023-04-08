f577a21a0113a3b11dda7985075add3024e79327
Rasaboun <rayane.amie@gmail.com>
2023-04-08 23:43:42 +0200

    Fix hydration error git add .git add .

--- weatherapp/components/TodayDashBoard.tsx
+++ weatherapp/components/TodayDashBoard.tsx
@@ -40,11 +40,7 @@ export  const TodayDashBoard = ({
 	}
 	const qualityColor = chooseQualityColor(actualHours, weatherData);
 	const colorAir = chooseAirQualityColor(actualHours, airData);
-  console.log("sunset is : ",sunset)
-  let parisdate = new Date(weatherData.daily.sunset[0]).getUTCHours() 
-  console.log("sunset hours is : ", parisdate)
-  console.log("sunset hours : ", new Date(weatherData.daily.sunset[0]).getUTCHours())
-  console.log("TimeZone is : ", weatherData.timezone)
+
 	return (
 		
 		<>
