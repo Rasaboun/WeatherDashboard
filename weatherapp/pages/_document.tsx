39e22a3fbf30b3b339c470ac2d73909f36df85c7
Rasaboun <rayane.amie@gmail.com>
2023-10-05 14:44:25 +0200

    style: update favicon

--- weatherapp/pages/_document.tsx
+++ weatherapp/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import img from "./../public/favicon.png"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head >
       <meta name="theme-color" content="rgb(219 234 254)" />
+      <link rel='shortcut icon' href='/favicon.ico' />
 
         </Head>
       <body className="bg-blue-100 bg-gradient-to-b from-blue-100  to-blue-400 min-h-screen">
