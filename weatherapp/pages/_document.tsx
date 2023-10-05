d42772a0310d8c5a1939adbc41647137e110d47c
Rasaboun <rayane.amie@gmail.com>
2023-10-05 15:06:52 +0200

    style: update favicon

--- weatherapp/pages/_document.tsx
+++ weatherapp/pages/_document.tsx
@@ -6,8 +6,8 @@ export default function Document() {
     <Html lang="en">
       <Head >
       <meta name="theme-color" content="rgb(219 234 254)" />
-      <link rel='shortcut icon' href='/favicon.ico' />
-
+      <link rel="icon" type="image/png" href="/favicon.png"/>
+      <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
         </Head>
       <body className="bg-blue-100 bg-gradient-to-b from-blue-100  to-blue-400 min-h-screen">
         <Main />
